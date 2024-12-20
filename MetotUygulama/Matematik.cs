8287213308079736e19ffa0b877776f3d7ab6c40
Halil Özcan <halil_ozcann@outlook.com>
2024-12-20 20:41:07 +0300

    C# Metot Uygulama Yapıldı

---
+++ MetotUygulama/Matematik.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetotUygulama
+{
+    public class Matematik
+    {
+        // Topla
+
+        public decimal toplamaIslemi(decimal sayi1,  decimal sayi2)
+        {
+            decimal sonuc = sayi1 + sayi2;
+            return sonuc;
+        }
+
+        // Cıkarma
+
+        public decimal cikarmaIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 - sayi2;
+            return sonuc;
+        }
+
+        // Bolme 
+        public decimal bolmeIslemi(decimal sayi1 ,decimal sayi2)
+        {
+            decimal sonuc = sayi1 / sayi2;
+            return sonuc;
+        }
+
+        // Carpma 
+
+        public decimal carpmaIslemi(decimal sayi1 ,  decimal sayi2)
+        {
+            decimal sonuc = sayi1 * sayi2;
+            return sonuc;
+        }
+
+        public void menuHazirla()
+        {
+            Console.Clear();
+            Console.WriteLine("************ Menu ***************");
+            Console.WriteLine("1- Toplama");
+            Console.WriteLine("2- Çıkarma");
+            Console.WriteLine("3- Çarpma");
+            Console.WriteLine("4-Bölme");
+            Console.Write("Seçiniz : ");
+
+        }
+
+        public void EkranaYazdir(decimal sayi1, decimal sayi2, decimal sonuc, string operators)
+        {
+            Console.WriteLine("{0} {1} {2} = {3}", sayi1, operators, sayi2, sonuc);
+        }
+    }
+}
--- MetotUygulama/Program.cs
+++ MetotUygulama/Program.cs
@@ -10,6 +10,55 @@ namespace MetotUygulama
     {
         static void Main(string[] args)
         {
+            Matematik matematik = new Matematik();
+            yenidenIslemYap:
+
+            matematik.menuHazirla();
+
+
+           int kullaniciSayi = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Lütfen Birinci Sayiyi Giriniz");
+            decimal kullaniciSayi1  = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lütfen İkinici Sayiyi Giriniz");
+            decimal kullaniciSayi2 = decimal.Parse(Console.ReadLine());
+
+            decimal sonuc = 0;
+
+            switch(kullaniciSayi)
+            {
+                case 1:
+                    sonuc = matematik.toplamaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    matematik.EkranaYazdir(kullaniciSayi, kullaniciSayi2, sonuc, "+");
+                    break;
+                case 2:
+                    sonuc = matematik.cikarmaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    matematik.EkranaYazdir(kullaniciSayi1, kullaniciSayi2, sonuc, "-");
+                    break;
+                case 3:
+                    sonuc = matematik.bolmeIslemi(kullaniciSayi1, kullaniciSayi2);
+                    matematik.EkranaYazdir(kullaniciSayi1, kullaniciSayi2, sonuc, "/");
+                    break;
+                case 4:
+                    sonuc = matematik.carpmaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    matematik.EkranaYazdir(kullaniciSayi1, kullaniciSayi2, sonuc, "*");
+                    break;
+                default:
+                    Console.WriteLine("Belirtmiş oldugunuz deger liste içerisinde bulunamadı");
+                    Console.WriteLine("Lütfen Yeniden Deneyiniz");
+                    System.Threading.Thread.Sleep(2000);
+                    goto yenidenIslemYap;
+            }
+
+            Console.WriteLine("Yeni işelm yapmak istiyor musunuz?");
+            string EH = Console.ReadLine();
+
+            if(EH.ToUpper() =="E")
+            {
+                goto yenidenIslemYap;
+            }
         }
     }
 }
